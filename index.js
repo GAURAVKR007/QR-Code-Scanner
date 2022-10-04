c57eb92f13d39aea9a8cbea6d5ad15682adcc988
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-10-04 18:58:23 +0530

    Functionality Added to Scan the QR via Api

---
+++ index.js
@@ -0,0 +1,46 @@
+const wrapper = document.querySelector('.wrapper')
+const form = document.querySelector('#form1')
+const fileInput = document.querySelector('#file')
+const infoText = document.getElementById('ptext')
+const textArea = document.getElementById('textArea')
+const copyBtn = document.querySelector('.copy')
+const closeBtn = document.querySelector('.close')
+
+function fetchRequest(formData, file){
+    infoText.innerText = "Scanning QR Code..."
+    fetch("http://api.qrserver.com/v1/read-qr-code/",{
+        method: "POST", body: formData
+    }).then(res => res.json()).then(result =>{
+        result = result[0].symbol[0].data
+        infoText.innerText = result ? "Upload QR Code to Scan" : "Couldn't Scan QR Code"
+        if(!result) return
+        textArea.innerText = result
+        form.querySelector("img").src = URL.createObjectURL(file)
+        wrapper.classList.add("active")
+        console.log(result);
+    }).catch(()=>{
+        infoText.innerText = "Couldn't Scan QR Code"
+    })
+}
+
+fileInput.addEventListener('change',e => {
+    let file = e.target.files[0];
+    if(!file) return;
+    let formData = new FormData()
+    formData.append("file",file)
+    fetchRequest(formData,file)
+})
+
+
+copyBtn.addEventListener("click",()=>{
+    let text = textArea.textContent;
+    navigator.clipboard.writeText(text)
+})
+
+form.addEventListener('click',()=>{
+    fileInput.click()
+})
+
+closeBtn.addEventListener("click",()=>{
+    wrapper.classList.remove("active")
+})
